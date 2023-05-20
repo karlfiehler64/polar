473eaa864416ef2e2e93af702072846582a93f06
Karl Fiehler <59343440+karlfiehler84@users.noreply.github.com>
2023-05-20 12:59:31 +0200

    improved object/walk sway and added movement restraints

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -4,6 +4,9 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const air_density : float = 1
+
+var current_direction : Vector3 
 
 var animation_weight : float = 10
 
@@ -23,25 +26,27 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if is_on_floor():
+		var input_dir = Input.get_vector("left", "right", "up", "down")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		current_direction = direction
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+	elif current_direction:
+		velocity.x = current_direction.x * SPEED
+		velocity.z = current_direction.z * SPEED
 
 	move_and_slide()
 	
-	smoothed_input = lerp(smoothed_input, input_dir, animation_weight * delta)
+	#divide velocity magnitude by max speed so the value is between 0 and 1 for blendtree
+	var walk_vector = velocity.length() / SPEED
 	
-
-	
-	print(velocity.z)
-	
+	animation_tree.set("parameters/walk_blend/blend_amount", walk_vector)
+		
 func _process(delta):
 	if Input.is_action_pressed("exit"):
 			get_tree().quit()
--- Scripts/WeaponManager.gd
+++ Scripts/WeaponManager.gd
@@ -4,6 +4,9 @@ extends Node3D
 
 @export var default_position : Vector3 
 
+var max_aim_sway : float = 40
+var max_walk_sway : Vector2 = Vector2(200,200)
+
 func _process(delta):
 	position.x = lerp(position.x, default_position.x, delta * 5)
 	position.z = lerp(position.z, default_position.z, delta * 5)
@@ -12,9 +15,14 @@ func _process(delta):
 	walk_sway(Vector2(input_dir.x, -input_dir.y))
 	
 func walk_sway(sway_amount):
+	#sway_amount = clamp(sway_amount, 0, max_walk_sway)
 	position.x += sway_amount.x * 0.005
 	position.z += sway_amount.y * 0.005
+	
 
 func aim_sway(sway_amount):
+	sway_amount.x = clamp(sway_amount.x, -max_aim_sway, max_aim_sway)
+	sway_amount.y = clamp(sway_amount.y, -max_aim_sway, max_aim_sway)
 	position.x += sway_amount.x * 0.0005
 	position.y += sway_amount.y * 0.0005
+	print(clamp(sway_amount.x, -max_aim_sway, max_aim_sway))
