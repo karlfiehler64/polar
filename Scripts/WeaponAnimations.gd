f774ec2218dcc0659f8bbb1d1a1c83df3f6b6d21
Karl Fiehler <k.fiehler64@gmail.com>
2023-05-22 13:44:41 +0200

    fixed shoot animation interfering with movement/weapon sway

--- Scripts/WeaponAnimations.gd
+++ Scripts/WeaponAnimations.gd
@@ -1,20 +1,21 @@
 extends Node3D
 
-@onready var current_object : Node3D = self.get_child(0)
-
+@onready var current_object : Node3D = $PlayerRigPlaceholder/M4A1
+@onready var camera = $".."
 @export var default_position : Vector3 
 @export var default_angle : Vector3
 
 var max_aim_sway : float = 40
 var max_walk_sway : Vector2 = Vector2(200,200)
 
+var input_dir : Vector2 = Vector2()
+
 func _process(delta):
 	position.x = lerp(position.x, default_position.x, delta * 5)
 	position.z = lerp(position.z, default_position.z, delta * 5)
 	position.y = lerp(position.y, default_position.y, delta * 5)
-	var input_dir = Input.get_vector("left", "right", "up", "down")
+	input_dir = Input.get_vector("left", "right", "up", "down")
 	walk_sway(Vector2(input_dir.x, -input_dir.y))
-	
 	
 func walk_sway(sway_amount):
 	#sway_amount = clamp(sway_amount, 0, max_walk_sway)
@@ -24,7 +25,9 @@ func walk_sway(sway_amount):
 func jump_sway(sway_amount):
 	position.y += sway_amount * 0.0005
 	
-
+func view_bobbing(frequency, strength, delta):
+	return
+	
 func aim_sway(sway_amount):
 	sway_amount.x = clamp(sway_amount.x, -max_aim_sway, max_aim_sway)
 	sway_amount.y = clamp(sway_amount.y, -max_aim_sway, max_aim_sway)
